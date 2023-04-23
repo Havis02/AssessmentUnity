ffce94ac12ff9751fe4725630f29b93f88faa673
Havis02 <holly2.havis@live.uwe.ac.uk>
2023-04-23 10:54:34 +0100

    Hover raycast and change scene script

--- Assets/Scripts/HoverDetect.cs
+++ Assets/Scripts/HoverDetect.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class HoverDetect : MonoBehaviour
 {
+    //public GameObject Inspection;
+    //public InspectionObject inspectionObj;
+    //public int index;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+  
     // Update is called once per frame
     void Update()
     {
-        
+        Ray ray = Camera.main.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (GetComponent<Collider>().Raycast(ray, out hit, 100f))
+        {
+            print("hover on" + gameObject.name);
+            SceneManager.LoadScene("TV", LoadSceneMode.Single);
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Inspection.SetActive(true);
+            }
+        }
     }
 }
