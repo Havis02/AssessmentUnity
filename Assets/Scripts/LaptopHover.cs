2f8a451a8358a257e1f33c6d8c4d72eeef2a27f2
Havis02 <holly2.havis@live.uwe.ac.uk>
2023-04-24 18:11:51 +0100

    Added scenechange and hover to interactions

---
+++ Assets/Scripts/LaptopHover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaptopHover : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (GetComponent<Collider>().Raycast(ray, out hit, 100f))
+        {
+            print("hover on" + gameObject.name);
+            SceneManager.LoadScene("Laptop", LoadSceneMode.Single);
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Inspection.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PianoHover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PianoHover : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (GetComponent<Collider>().Raycast(ray, out hit, 100f))
+        {
+            print("hover on" + gameObject.name);
+            SceneManager.LoadScene("Piano", LoadSceneMode.Single);
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Inspection.SetActive(true);
+            }
+        }
+    }
+}
